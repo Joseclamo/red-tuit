d80b4e07621fddf18af48161eb3cc04c18a24f8f
Jose <joseclamo@gmail.com>
2018-03-09 14:16:13 +0100

    validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,8 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+
+  validates :message, presence: true
+  validates :message, length: {maximum: 140, 
+  too_long: "Un tweet sólo tiene 140 caracteres. ¡Todo el mundo lo sabe!"}
+
 end
